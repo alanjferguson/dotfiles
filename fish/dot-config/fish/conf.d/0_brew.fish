c9e4418734bb8db8ff9d63c1e36022e0889633b4
Alan J. Ferguson <alan@ferguson.info>
2025-01-14 19:41:10 +0000

    Update brew conf for fish

--- fish/dot-config/fish/conf.d/0_brew.fish
+++ fish/dot-config/fish/conf.d/0_brew.fish
@@ -1,4 +1,35 @@
-set -l brew_path "/opt/homebrew/bin/brew"
-if test -x $brew_path
-    eval "$($brew_path shellenv)"
+# This script sets up brew for fish shell
+# I've tried to do it for both linux and macos
+
+# get right path for brew
+switch (uname)
+case Darwin
+  if test -x "/usr/local/bin/brew"
+    set brew_path "/usr/local/bin/brew"
+  else if test -x "/opt/homebrew/bin/brew"
+    set brew_path "/opt/homebrew/bin/brew"
+  end
+case Linux
+  set brew_path "/home/linuxbrew/.linuxbrew/bin/brew"
+end
+
+# check brew executable exists
+if not test -x $brew_path
+  echo "brew not found"
+  exit 1
+end
+
+# add brew to path
+if not $brew_path shellenv | source
+  echo "brew shellenv failed"
+  exit 1
+end
+
+# set up compleitions
+if test -d (brew --prefix)"/share/fish/completions"
+  set -p fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+  set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
 end
