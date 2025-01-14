dc6c03325e4b3980b87e4a8916dfa46ddec241e7
Alan J. Ferguson <alan@ferguson.info>
2025-01-14 23:34:11 +0000

    Update fish greeting

--- fish/dot-config/fish/functions/fish_greeting.fish
+++ fish/dot-config/fish/functions/fish_greeting.fish
@@ -1,5 +1,8 @@
 function fish_greeting
   if type -q pfetch
-    PF_INFO="ascii title os host kernel uptime memory" pfetch
+    PF_INFO='ascii os host kernel uptime cpu memory shell editor palette' pfetch
+    echo -ne "\033[F" # used to hide extra space at end
+  else
+    echo "Welcome to fish, the friendly interactive shell"
   end
 end
