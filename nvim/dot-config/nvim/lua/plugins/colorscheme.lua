f6a5e58ddb8840b0340a7b95c91835f76db2e754
Alan J. Ferguson <alan@ferguson.info>
2025-01-16 17:52:27 +0000

    Update nvim to use monokai pro

--- nvim/dot-config/nvim/lua/plugins/colorscheme.lua
+++ nvim/dot-config/nvim/lua/plugins/colorscheme.lua
@@ -4,22 +4,23 @@ return {
     depends = "navarasu/onedark.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-    dependencies = { { 'navarasu/onedark.nvim', }, },
+    dependencies = { 
+      { 
+        'loctvl842/monokai-pro.nvim',
+        setup = function()
+          require("monokai-pro").setup({})
+        end,
+      },
+    },
     opts = {
       update_interval = 1000,
       set_dark_mode = function()
         vim.api.nvim_set_option_value("background", "dark", {})
-        require('onedark').setup({
-          style = 'dark'
-        })
-        vim.cmd('colorscheme onedark')
+        vim.cmd("colorscheme monokai-pro")
       end,
       set_light_mode = function()
         vim.api.nvim_set_option_value("background", "light", {})
-        require('onedark').setup({
-          style = 'light'
-        })
-        vim.cmd('colorscheme onedark')
+        vim.cmd("colorscheme monokai-pro-light")
       end,
     },
   }
