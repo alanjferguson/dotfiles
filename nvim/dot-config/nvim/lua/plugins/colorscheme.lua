13a953e708cc6ce1df40f113ff45a2d6442c579b
Alan J. Ferguson <alan@ferguson.info>
2025-01-16 16:51:43 +0000

    Tidy up nvim

--- nvim/dot-config/nvim/lua/plugins/colorscheme.lua
+++ nvim/dot-config/nvim/lua/plugins/colorscheme.lua
@@ -1,29 +1,26 @@
 return {
   {
-    'projekt0n/github-nvim-theme',
-    name = 'github-theme',
-    -- lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    -- priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require('github-theme').setup({
-        -- ...
-      })
-
-      vim.cmd('colorscheme github_dark_default')
-    end,
-  },
-  {
-  "f-person/auto-dark-mode.nvim",
-  opts = {
-    update_interval = 1000,
-    set_dark_mode = function()
-      vim.api.nvim_set_option_value("background", "dark", {})
-      vim.cmd("colorscheme github_dark_default")
-    end,
-    set_light_mode = function()
-      vim.api.nvim_set_option_value("background", "light", {})
-      vim.cmd("colorscheme github_light_default")
-    end,
-  },
+    "f-person/auto-dark-mode.nvim",
+    depends = "navarasu/onedark.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    dependencies = { { 'navarasu/onedark.nvim', }, },
+    opts = {
+      update_interval = 1000,
+      set_dark_mode = function()
+        vim.api.nvim_set_option_value("background", "dark", {})
+        require('onedark').setup({
+          style = 'dark'
+        })
+        vim.cmd('colorscheme onedark')
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option_value("background", "light", {})
+        require('onedark').setup({
+          style = 'light'
+        })
+        vim.cmd('colorscheme onedark')
+      end,
+    },
   }
 }
