1c5d9110b0bafa3151379bf60ced32010d0a08f6
Alan J. Ferguson <alan@ferguson.info>
2025-02-04 14:36:00 +0000

    Update so we use nvim or code as editor in fish

--- fish/dot-config/fish/conf.d/editor.fish
+++ fish/dot-config/fish/conf.d/editor.fish
@@ -1,2 +1,9 @@
-set -gx EDITOR nvim
-set -gx VISUAL nvim
+if test "$TERM_PROGRAM" = "vscode"
+    set -gx EDITOR code --wait
+    set -gx VISUAL code --wait
+else if type -q nvim
+    set -gx EDITOR nvim
+    set -gx VISUAL nvim
+else
+    echo "Could not set editor!"
+end
