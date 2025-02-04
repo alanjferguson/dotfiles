a035a9350d065a9aaf10b6d3b1f62534986c86e0
Alan J. Ferguson <alan@ferguson.info>
2025-02-04 13:38:20 +0000

    Update nvim

--- nvim/dot-config/nvim/lua/plugins/colorscheme.lua
+++ nvim/dot-config/nvim/lua/plugins/colorscheme.lua
@@ -1,26 +1,23 @@
 return {
   {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  },
+  {
     "f-person/auto-dark-mode.nvim",
-    depends = "navarasu/onedark.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    dependencies = { 
-      { 
-        'loctvl842/monokai-pro.nvim',
-        setup = function()
-          require("monokai-pro").setup({})
-        end,
-      },
-    },
+    priority = 999, -- make sure to load this before all the other start plugins
     opts = {
       update_interval = 1000,
       set_dark_mode = function()
         vim.api.nvim_set_option_value("background", "dark", {})
-        vim.cmd("colorscheme monokai-pro")
+        vim.cmd("colorscheme tokyonight-night")
       end,
       set_light_mode = function()
         vim.api.nvim_set_option_value("background", "light", {})
-        vim.cmd("colorscheme monokai-pro-light")
+        vim.cmd("colorscheme tokyonight-day")
       end,
     },
   }
