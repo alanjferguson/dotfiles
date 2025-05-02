3534885101030e558fc4eb12a2ac3049243ed82a
Alan J. Ferguson <alan@ferguson.info>
2025-05-02 16:07:16 +0100

    Refactor WezTerm configuration for clarity and consistency

--- wezterm/dot-config/wezterm/wezterm.lua
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -1,5 +1,7 @@
+-- WezTerm configuration
 local wezterm = require("wezterm")
 
+-- Create a config object using the builder pattern
 local config = wezterm.config_builder()
 
 -- wezterm.gui is not available to the mux server, so take care to
@@ -12,27 +14,28 @@ local function get_appearance()
 end
 
 local function scheme_for_appearance(appearance)
-  if appearance:find 'Dark' then
+  if appearance:find('Dark') then
     return 'tokyonight_night'
   else
     return 'tokyonight_day'
   end
 end
 
--- config.color_scheme_dirs = { '~/.config/wezterm/colors' }
+-- Set the color scheme based on system appearance
 config.color_scheme = scheme_for_appearance(get_appearance())
 
+-- Make the cursor easier to see by using reverse video style
 config.force_reverse_video_cursor = true
 
 -- Font setup
 config.font_size = 14.0
 config.font = wezterm.font({
-	family = "JuliaMono Nerd Font",
+  family = "JuliaMono Nerd Font",
 })
+-- Use slashed zero for better readability
 config.harfbuzz_features = { 'zero' }
 
-
--- Use retro text tab bar instead
+-- Use the retro text tab bar style instead of the fancy one
 config.use_fancy_tab_bar = false
 
 return config
