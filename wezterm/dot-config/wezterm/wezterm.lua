e1f7f1d97dc4728a3b2712918a900e0e13414015
Alan J. Ferguson <alan@ferguson.info>
2025-06-30 13:57:48 +0100

    Remove fullscreen keybinding in wezterm
    Default ALT+Enter keybinding clashes with TUI apps such as lazygit

--- wezterm/dot-config/wezterm/wezterm.lua
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -7,18 +7,18 @@ local config = wezterm.config_builder()
 -- wezterm.gui is not available to the mux server, so take care to
 -- do something reasonable when this config is evaluated by the mux
 local function get_appearance()
-  if wezterm.gui then
-    return wezterm.gui.get_appearance()
-  end
-  return 'Dark'
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	return "Dark"
 end
 
 local function scheme_for_appearance(appearance)
-  if appearance:find('Dark') then
-    return 'Catppuccin Mocha'
-  else
-    return 'Catppuccin Latte'
-  end
+	if appearance:find("Dark") then
+		return "Catppuccin Mocha"
+	else
+		return "Catppuccin Latte"
+	end
 end
 
 -- Set the color scheme based on system appearance
@@ -27,13 +27,22 @@ config.color_scheme = scheme_for_appearance(get_appearance())
 -- Font setup
 config.font_size = 14.0
 config.font = wezterm.font({
-  family = "Iosevka Nerd Font",
+	family = "JuliaMono Nerd Font",
 })
 -- Use slashed zero for better readability
-config.harfbuzz_features = { 'zero' }
+config.harfbuzz_features = { "zero" }
 
 -- Use the retro text tab bar style instead of the fancy one
 config.use_fancy_tab_bar = false
+
+-- Disable fullscreen mode keybindings
+config.keys = {
+	{
+		key = "Enter",
+		mods = "ALT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+}
 
 return config
 
